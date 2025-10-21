b5e2a39f3c9f90af8a5ba5633bdf8389547244d0
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-10-21 17:40:28 +0300

    Create Alistirma2Cevap.cs

---
+++ Alıştırmalar/Hafta-3/Alistirma2Cevap.cs
@@ -0,0 +1,47 @@
+namespace Alistirma2Cevap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Klavyeden girilen 3 basamaklı pozitif bir tam sayının basamakları toplamını bulanprogramı yazınız.
+            Ör: 123 sayısının basamakları toplam 1 + 2 + 3 = 6’dır.
+
+            Not: 
+            Hatalı (3 basamaklı olmayan) bir sayı girildiğinde kullanıcıya uyarı verilir 
+            ve program doğru giriş yapılana kadar tekrar çalışmaya devam eder.
+            */
+
+            while (true)
+            {
+            Console.Write("3 basamaklı bir tam sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi < 100)
+            {
+                Console.WriteLine("3 basamaklı bir tam sayı girmeniz gerekmekte!");
+                Thread.Sleep(3000);
+                Console.Clear();
+
+            }
+            else if (sayi > 999)
+            {
+                Console.WriteLine("3 basamaklı bir tam sayı girmeniz gerekmekte!");
+                Thread.Sleep(3000);
+                Console.Clear();
+                }
+            else
+            {
+                int yuzler = sayi / 100;
+                int onlar = (sayi % 100)/10;
+                int birler = sayi % 10;
+                Console.WriteLine("Girdiğiniz sayının;");
+                Console.WriteLine("Yüzler basamağı: " + yuzler);
+                Console.WriteLine("Onlar basamağı: " + onlar);
+                Console.WriteLine("Birler basamağı: " + birler);
+                break;
+            }
+            }
+        }
+    }
+}
