ea74a35f5207d9ccbec216c6fd217331dad20188
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-10-21 23:03:38 +0300

    Update Alistirma1Cevap.cs

--- Alıştırmalar/Hafta-4/Alistirma1Cevap.cs
+++ Alıştırmalar/Hafta-4/Alistirma1Cevap.cs
@@ -1,1 +1,19 @@
-
+namespace Hafta4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            İlk 10 doğal sayının toplamını bulan bir C# programı yazın.
+            */
+          
+            int toplam = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine(toplam);
+        }
+    }
+}
