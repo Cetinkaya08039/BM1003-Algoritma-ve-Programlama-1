3063d57291e60c69c68390f7ee764cca49bb0169
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-10-21 23:12:43 +0300

    Update Alistirma2Cevap.cs

--- Alıştırmalar/Hafta-4/Alistirma2Cevap.cs
+++ Alıştırmalar/Hafta-4/Alistirma2Cevap.cs
@@ -1,11 +1,24 @@
-namespace Cevap
+namespace Alistirma2Cevap
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Çözümlerinizi buraya yazın.
-            Console.WriteLine("Hello World!");
+            /*
+            10 sayıyı kullanıcıdan alıp bunların ortalamasını ve toplamını bulan bir C# programı yazın.
+            */
+
+            int[] sayi = new int[10];
+            double toplam = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write((i +1) + ". sayıyı giriniz: ");
+                sayi[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += sayi[i];
+                Console.Clear();
+            }
+            Console.WriteLine("Sayıların toplamı = " + toplam + ", Ortalaması = " + toplam/10);
+
         }
     }
 }
