c9145830cf2641ac2eacffa3b7d15723f7483d67
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-10-23 12:25:46 +0300

    Update Alistirma1Cevap.cs

--- Alıştırmalar/Hafta-5/Alistirma1Cevap.cs
+++ Alıştırmalar/Hafta-5/Alistirma1Cevap.cs
@@ -1,11 +1,25 @@
-namespace Cevap
+namespace Alistirma1Cevap
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Çözümlerinizi buraya yazın.
-            Console.WriteLine("Hello World!");
+            /*
+            10 elemanı bir tam sayı dizisini kullanıcıdan alınan değerlerle dolduran ve dizideki elemanları yazdıran kodu yazınız.
+            */
+
+            double[] dizi = new double[10];
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.Write("Dizinin " + (i+1) + ". elemanını giriniz: ");
+                dizi[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.Write(dizi[i] + " ");
+            }
+
         }
     }
 }
