d34f764aff25d656ae8b87633ed96e47e43762e5
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-10-23 12:28:53 +0300

    Update Alistirma2Cevap.cs

--- Alıştırmalar/Hafta-5/Alistirma2Cevap.cs
+++ Alıştırmalar/Hafta-5/Alistirma2Cevap.cs
@@ -1,11 +1,26 @@
-namespace Cevap
+namespace Alistirma2Cevap
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Çözümlerinizi buraya yazın.
-            Console.WriteLine("Hello World!");
+            /*
+            Rastgele sayılarla oluşturulan 20 elemanlı bir dizinin, elemanlarından kaç tanesinin dışarıdan girilen bir sayıdan küçük olduğunu bulunuz.
+            */
+            int[] dizi = new int[20];
+            Console.Write("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            int miktar = 0;
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = rnd.Next(0,100);
+                if (dizi[i] < sayi)
+                {
+                    miktar++;
+                }
+            }
+            Console.WriteLine(miktar);
         }
     }
 }
