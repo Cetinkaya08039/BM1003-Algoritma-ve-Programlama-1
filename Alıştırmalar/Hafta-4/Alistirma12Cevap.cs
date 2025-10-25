36c500c7f4acaeb19efc9dc39bf60c8d2361fc2d
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-10-25 16:37:46 +0300

    Create Alistirma12Cevap.cs

---
+++ Alıştırmalar/Hafta-4/Alistirma12Cevap.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace Alistirma12Cevap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Girilen bir tam sayının basamaklarındaki en büyük rakamı bulan programı yazınız.
+            */
+
+            Console.Write("Bir tam sayı giriniz: ");
+            string sayistr = Console.ReadLine();
+            int sayiint = Convert.ToInt32(sayistr);
+            int basamaksayi = sayistr.Length;
+            int[] basamak = new int[basamaksayi];
+            int buyuk = 0;
+            for (int i = 0; i < basamaksayi; i++)
+            {
+                if (i == 0)
+                {
+                    // Convert.ToInt32 sayıyı yuvarlıyordu örneğin sayı 5.6 ise 5 çıkartmak yerine 6 yapıyordu bunun yerine (int) kullanıldı.
+                    basamak[i] = (int)(sayiint / Math.Pow(10, basamaksayi - 1));
+                    if (basamak[i] >buyuk)
+                    {
+                        buyuk = basamak[i];
+                    }
+                }
+                else if (i == basamaksayi - 1)
+                {
+                    basamak[i] = sayiint % 10;
+                    if (basamak[i] > buyuk)
+                    {
+                        buyuk = basamak[i];
+                    }
+                }
+                else
+                {
+                    basamak[i] = (int)(sayiint % Math.Pow(10, basamaksayi - i) / Math.Pow(10, basamaksayi - 1 - i));
+                    if (basamak[i] > buyuk)
+                    {
+                        buyuk = basamak[i];
+                    }
+                }
+                
+            }
+            Console.WriteLine("Girdiğiniz sayıların rakamlarının en büyüğü: " + buyuk);
+        }
+
+            
+
+            
+        
+    }
+}
