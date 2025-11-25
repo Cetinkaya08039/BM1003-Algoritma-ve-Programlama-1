e891571fbe833954bf3151e088d2fd4366c14de2
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-11-25 13:48:29 +0300

    Create Alistirma18Cevap.cs

---
+++ Alıştırmalar/Hafta-5/Alistirma18Cevap.cs
@@ -0,0 +1,18 @@
+namespace Alistirma18Cevap.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Onluk tabanda bir sayıyı ikili sayıya çeviren bir program yazınız.
+            Giriş Verileri:
+            Bir Sayı Girin: 5
+            Beklenen çıktı:
+            İkili sayı: 101
+            */
+
+        }
+        
+    }
+}
