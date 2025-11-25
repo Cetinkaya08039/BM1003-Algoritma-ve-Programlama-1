56423d2eed32d685b8bee2b9170784cd07249de9
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2025-11-25 16:52:15 +0300

    Update Alistirma1Cevap.cs

--- Alıştırmalar/Hafta-8/Alistirma1Cevap.cs
+++ Alıştırmalar/Hafta-8/Alistirma1Cevap.cs
@@ -1,1 +1,23 @@
+namespace Alistirma1Cevap
+{
+    internal class Program
+    {
+        static void toplam()
+        {
+            Console.Write("Birinci sayıyı giriniz: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Toplam: " + (x*x + y*y));
+        }
+        static void Main(string[] args)
+        {
+            /*
+            Kullanıcının girdiği 2 sayının karelerinin toplamını hesaplayıp geri döndüren bir metot yazınız
+            */
+            toplam();
+
+        }
+    }
+}
 
